5f7426baf510c83ff49bb0a4a6b780617159d998
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-05-20 19:47:41 +0530

    Review system

--- src/prisma.js
+++ src/prisma.js
@@ -5,7 +5,19 @@ const prisma = new Prisma({
 	endpoint: "http://localhost:4466",
 });
 
+// prisma.exists
+// 	.Comment({
+// 		id: "abc123",
+// 	})
+// 	.then((exists) => {
+// 		console.log(exists);
+// 	});
+
 const createPostForUser = async (authorId, data) => {
+	const userExists = await prisma.exists.User({ id: authorId });
+	if (!userExists) {
+		throw new Error("User not found!");
+	}
 	const post = await prisma.mutation.createPost(
 		{
 			data: {
@@ -17,23 +29,19 @@ const createPostForUser = async (authorId, data) => {
 				},
 			},
 		},
-		"{ id }"
+		"{ author { id name email posts { id title published }} }"
 	);
-	const user = await prisma.query.user(
-		{
-			where: {
-				id: authorId,
-			},
-		},
-		"{ id name email posts { id title published }}"
-	);
-	return user;
+	return post.author;
 };
 
 // createPostForUser("ckac1rry60009084441n85jwm", {
-// 	title: "React Native Course",
+// 	title: "React Native Course v2",
 // 	body: "Udemy has best courses for it",
 // 	published: true,
-// }).then((user) => {
-// 	console.log(JSON.stringify(user, undefined, 2));
-// });
+// })
+// 	.then((user) => {
+// 		console.log(JSON.stringify(user, undefined, 2));
+// 	})
+// 	.catch((error) => {
+// 		console.log(error.message);
+// 	});
