f74c4ab0a350f907800f0e516ad41ab65cab2cbc
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-05-30 15:56:37 +0530

    Create and save hashed password

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,12 +1,22 @@
+import bcrypt from "bcryptjs";
+
 const Mutation = {
 	async createUser(parent, args, { prisma }, info) {
-		const emailTaken = await prisma.exists.User({ email: args.data.email });
-
-		if (emailTaken) {
-			throw new Error("Email Taken");
+		if (args.data.password.length < 8) {
+			throw new Error("Password must be 8 characters or longer");
 		}
 
-		return prisma.mutation.createUser({ data: args.data }, info);
+		const password = await bcrypt.hash(args.data.password, 10);
+
+		return prisma.mutation.createUser(
+			{
+				data: {
+					...args.data,
+					password,
+				},
+			},
+			info
+		);
 	},
 	async deleteUser(parent, args, { prisma }, info) {
 		const userExists = await prisma.exists.User({ id: args.id });
